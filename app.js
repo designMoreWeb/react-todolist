c9dc4b169496931481a8963bb96d8349985a1192
designMoreWeb <divystolia@gmail.com>
2017-01-15 16:42:19 -0500

    Added a toggle all complete button with React Native touchable opacity controls

--- app.js
+++ app.js
@@ -7,10 +7,24 @@ class App extends Component{
    constructor(props) {
      super(props);
      this.state = {
+      allComplete:false,
       value:"",
       items:[]
      }
      this.handleAddItem = this.handleAddItem.bind(this);
+     this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+   }
+   handleToggleAllComplete(){
+      const complete = !this.state.allComplete;
+      const newItems = this.state.items.map((item) =>({
+         ...item,
+         complete
+      }))
+      console.table(newItems);
+      this.setState({
+         items: newItems,
+         allComplete: complete
+      })
    }
    handleAddItem(){
       if(!this.state.value) return;
@@ -34,6 +48,7 @@ class App extends Component{
                value={this.state.value}
                onAddItem={this.handleAddItem}
                onChange={(value) => this.setState({ value })}
+               onToggleAllComplete={this.handleToggleAllComplete}
             />
                <View style={styles.content}>
 
@@ -47,7 +62,7 @@ class App extends Component{
 const styles =StyleSheet.create({
    container:{
       flex: 1,
-      backgroundColor:"#57bfa7",
+      backgroundColor:"#555555",
       ...Platform.select({
          ios:{ paddingTop:30 }
       })
--- header.js
+++ header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 class Header extends Component{
    render() {
       return(
          <View style={styles.header}>
+            <TouchableOpacity onPress={this.props.onToggleAllComplete}>
+               <Text style={styles.toggleIcon}>{String.fromCharCode(10003)}</Text>
+            </TouchableOpacity>
             <TextInput
                value={this.props.value}
                onChangeText={this.props.onChange}
@@ -25,8 +28,13 @@ const styles =StyleSheet.create({
       justifyContent:"space-around",
       alignItems:"center"
    },
+      toggleIcon:{
+         fontSize: 30,
+         color:"#57bfa7"
+      },
    input:{
       flex: 1,
+      marginLeft:15,
       height:50
    }
 })
