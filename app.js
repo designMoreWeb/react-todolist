945ca2d836060e096db8d13704a4556dd2ddb7f4
designMoreWeb <divystolia@gmail.com>
2017-01-15 15:36:06 -0500

    added views and cleaned up UI

---
+++ app.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import Header from './header';
+import Footer from './footer';
+
+class App extends Component{
+   render() {
+      return(
+         <View style={styles.container}>
+            <Header />
+               <View style={styles.content}>
+
+               </View>
+            <Footer />
+         </View>
+         );
+   }
+}
+
+const styles =StyleSheet.create({
+   container:{
+      flex: 1,
+      backgroundColor:"#57bfa7",
+      ...Platform.select({
+         ios:{ paddingTop:30 }
+      })
+   },
+   content:{
+      flex:1
+   }
+
+})
+
+export default App;
